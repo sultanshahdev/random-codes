eb4a9fcb5444bad398a2d4ad8d05e7f2d4e34925
sultan <sultanshah.bscsf24@gmail.com>
2026-05-25 17:41:38 +0500

    Add new File

---
+++ newjsfunctions.js
@@ -0,0 +1,19 @@
+const Book=
+	function(title,author,numOfPages,readStatus)
+	{
+		this.title = String(title);
+		this.author= String(author);
+		this.numOfPages = Number(numOfPages);
+		this.readStatus = readStatus;
+		this.info = function()
+		{
+			return `${this.title}, by ${this.author} , ${this.numOfPages}, ${this.readStatus}`;
+		}
+
+
+	}
+
+// random books
+
+let book1 = new Book("harry-potter","jk-rowling",233,'read');
+console.log(book1.info());
